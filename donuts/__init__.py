b62cb3b8eb338feac3190ded7e22fc36c492ae87
jmccormac01 <jmccormac001@gmail.com>
2016-03-19 14:54:04 +0000

    Initial layout for package

---
+++ donuts/__init__.py
@@ -0,0 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from astropy.io import fits
+    from scipy.fftpack import fft, ifft
+    from scipy import ndimage, conjugate, polyfit, polyval
+    import numpy as np  
