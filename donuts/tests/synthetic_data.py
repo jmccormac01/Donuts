c8a92acac1f08c3956ddf7ba6c9581181397d664
Simon Walker <s.r.walker101@googlemail.com>
2016-03-26 20:09:10 +0000

    Add docstrings

--- donuts/tests/synthetic_data.py
+++ donuts/tests/synthetic_data.py
@@ -1,11 +1,18 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter
 
 
 def generate_background(shape, background_level, background_sigma):
+    '''
+    Generates a Gaussian background signal, with the supplied level and sigma
+    '''
     return np.random.normal(background_level, background_sigma, size=shape)
 
 
 def generate_signals(shape, positions):
+    '''
+    Generates delta functions at the specified positions
+    '''
     out = np.zeros(shape)
     for position in positions:
         x, y = position
@@ -14,6 +21,9 @@ def generate_signals(shape, positions):
 
 
 def generate_synthetic_data(shape=(1024, 1024), background_level=100, background_sigma=5):
+    '''
+    Generates synthetic data, consisting of a flat gaussian background, with Gaussians for stars.
+    '''
     return generate_background(
         shape=shape,
         background_level=background_level,
