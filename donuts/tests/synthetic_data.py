368dfbd6244c126b23b3bbf1d37398d0156f5dd2
Simon Walker <s.r.walker101@googlemail.com>
2016-03-26 19:49:35 +0000

    Generate backgound

--- donuts/tests/synthetic_data.py
+++ donuts/tests/synthetic_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 
-def generate_synthetic_data(shape):
-    return np.zeros(shape)
+def generate_synthetic_data(shape=(1024, 1024), background_level=100, background_sigma=5):
+    background = np.random.normal(background_level, background_sigma, size=shape)
+    return background
--- donuts/tests/test_synthetic_data.py
+++ donuts/tests/test_synthetic_data.py
@@ -4,3 +4,17 @@ from .synthetic_data import generate_synthetic_data
 def test_generate_shape():
     data = generate_synthetic_data(shape=(10, 10))
     assert data.shape == (10, 10)
+
+
+def test_background_level():
+    background_level = 100
+    background_sigma = 0.01
+    data = generate_synthetic_data(
+        background_level=background_level,
+        background_sigma=background_sigma,
+    )
+
+    expected_min = 95
+    expected_max = 105
+    assert expected_min < data[0][0] < expected_max
+
