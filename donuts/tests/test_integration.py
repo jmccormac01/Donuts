376d02eaced9c9c8c8ee73b2b9c4f217f36686c0
jmccormac01 <jmccormac001@gmail.com>
2016-03-28 00:24:47 +0100

    adding testdata and updating test_integration

--- donuts/tests/test_integration.py
+++ donuts/tests/test_integration.py
@@ -1,16 +1,38 @@
+"""Real world test of Donuts using real images from NGTS
+"""
+import os.path
 from ..donuts import Donuts
 
+ROOT_PATH = os.path.dirname(__file__)
+DATA_DIR = os.path.join(ROOT_PATH, '..', 'data')
 
 # Test the donuts code using real data
 def test_full_integration():
+    """Test real world usage of Donuts with real data
+    
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
     # initialise the class with the settings needed
     # upon generation of the reference image
-    d = Donuts(refimage='IMAGE80520160114005507.fits', image_ext=0, exposure='EXPOSURE',
-               normalise=True, subtract_bkg=True, prescan_width=20, overscan_width=20, boarder=64, ntiles=32)
+    test_ref_image = os.path.join(DATA_DIR, 'IMAGE80520160114005507.fits')
+    d = Donuts(refimage=test_ref_image, image_ext=0, exposure='EXPOSURE',
+               normalise=True, subtract_bkg=True, prescan_width=20,
+               overscan_width=20, boarder=64, ntiles=32)
     # print a summary of the setup
     d.print_summary()
     # assumes all the settings from the ref image generation
     # and calculates the shift between the images
     imlist = ['IMAGE80520160114005520.fits', 'IMAGE80520160114005533.fits']
     for image in imlist:
-        x, y = d.measure_shift(checkimage=image)
+        test_check_image = os.path.join(DATA_DIR, image)
+        x, y = d.measure_shift(checkimage=test_check_image)
