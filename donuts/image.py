a80dc1c5df8706c15d15bf5f3691b7ed2aa6a245
Simon Walker <s.r.walker101@googlemail.com>
2016-05-03 21:31:47 +0100

    Add tests for #trim

--- donuts/image.py
+++ donuts/image.py
@@ -1,6 +1,48 @@
+import numpy as np
+
+
 class Image(object):
     '''Encapsulate the transformations applied to images
     '''
+
     def __init__(self, data, header):
         self.raw_image = data
+        self.raw_region = None
+        self.sky_background = None
+        self.backsub_region = None
         self.header = header
+        self.proj_x = None
+        self.proj_y = None
+        self.x = None
+        self.y = None
+
+    def trim(self, prescan_width=0, overscan_width=0, border=64):
+        if overscan_width > 0 and prescan_width > 0:
+            image_section = self.raw_image[:, prescan_width:-overscan_width]
+        elif overscan_width > 0:
+            image_section = self.raw_image[:, :-overscan_width]
+        elif prescan_width > 0:
+            image_section = self.raw_image[:, prescan_width:]
+        else:
+            image_section = self.raw_image
+
+        dy, dx = image_section.shape  
+
+        # check if the CCD is a funny shape. Normal CCDs should divide by 16
+        # with no remainder. NITES for example does not (1030,1057)
+        # instead of (1024,1024)
+        rx = dx % 16
+        ry = dy % 16
+        base = 512
+
+        if rx > 0 or ry > 0:
+            dimx = int(dx // base) * base
+            dimy = int(dy // base) * base
+        else:
+            dimx = dx
+            dimy = dy
+
+        # get the reference data, with tweaked shape if needed
+        self.raw_region = image_section[
+            border:dimy - border, border:dimx - border
+        ]
--- donuts/tests/test_image.py
+++ donuts/tests/test_image.py
@@ -1,7 +1,57 @@
+import pytest
+import numpy as np
+
 from donuts.image import Image
+
+
+def generate_image(shape):
+    return np.random.randint(1500, 2**16 - 1, size=shape).astype(
+        np.uint16)
+
+
+@pytest.fixture(scope='module')
+def ngts_data():
+    return generate_image((2048, 2048))
+
+
+def compute_projections(arr):
+    return np.sum(arr, axis=0), np.sum(arr, axis=1)
 
 
 def test_image_stores_parameters():
     image = Image(data='a', header='b')
     assert image.raw_image == 'a'
     assert image.header == 'b'
+
+
+def test_image_has_default_none_for_xy():
+    image = Image(data='a', header='b')
+    assert image.x is None and image.y is None
+
+
+class TestImageShapeReading(object):
+
+    def test_without_border_or_overscans(self):
+        image = Image(generate_image((2048, 2048)), None)
+        image.trim(border=0)
+        assert image.raw_region.shape == (2048, 2048)
+
+    def test_with_border_no_overscan(self):
+        image = Image(generate_image((2048, 2048)), None)
+        image.trim(border=64)
+        assert image.raw_region.shape == (1920, 1920)
+
+    def test_with_border_and_overscans(self):
+        image = Image(generate_image((2048, 2048)), None)
+        image.trim(border=64, overscan_width=20, prescan_width=20)
+        assert image.raw_region.shape == (1920, 1408)
+
+    def test_overscan_only(self):
+        image = Image(generate_image((2048, 2048)), None)
+        image.trim(border=0, overscan_width=20)
+        assert image.raw_region.shape == (2048, 1536)
+
+    def test_prescan_only(self):
+        image = Image(generate_image((2048, 2048)), None)
+        image.trim(border=0, prescan_width=64)
+        assert image.raw_region.shape == (2048, 1984)
