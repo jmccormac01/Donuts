8c6f9246759ceab7f8dc0d885fadb9734b11402d
James McCormac <jmccormac001@gmail.com>
2016-06-02 15:06:18 +0100

    bumping version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ LONG_DESCRIPTION = open('README.rst').read()
 builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.0.dev'
+VERSION = '0.1.0'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
