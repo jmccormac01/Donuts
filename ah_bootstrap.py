cda01696a8f874676b3239fb2ec3b5ab0ea9d5ef
James McCormac <jmccormac001@gmail.com>
2017-10-23 17:21:24 +0100

    adding new helpers

--- ah_bootstrap.py
+++ ah_bootstrap.py
@@ -90,6 +90,14 @@ except:
     from ez_setup import use_setuptools
     use_setuptools()
 
+#typing as a dependency for 1.6.1+ Sphinx causes issues when imported after
+# initializing submodule with ah_boostrap.py
+# See discussion and references in
+# https://github.com/astropy/astropy-helpers/issues/302
+try:
+    import typing # noqa
+except ImportError:
+    pass
 
 # Note: The following import is required as a workaround to
 # https://github.com/astropy/astropy-helpers/issues/89; if we don't import this
