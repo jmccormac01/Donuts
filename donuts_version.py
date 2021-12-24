58eb0657a0304f802308c53f4cc20a28ac0f7ea4
James McCormac <jmccormac001@gmail.com>
2021-12-24 12:23:02 +0000

    bumps version and minor update to notebook

--- donuts_version.py
+++ donuts_version.py
@@ -1,7 +1,7 @@
 from astropy_helpers.git_helpers import get_git_devstr
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.3.2'
+VERSION = '0.3.3'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
